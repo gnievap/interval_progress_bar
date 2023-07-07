c60f09bc3b00e6fb95f8e279241f356d9c72dbc0
Gaby Nieva <gaby.nieva@outlook.com>
2023-07-06 23:20:54 -0600

    Adding more intervals

--- lib/interval_progress_bar.dart
+++ lib/interval_progress_bar.dart
@@ -23,31 +23,62 @@ class _IntervalProgressBarState extends State<IntervalProgressBar> {
   }
 
   Widget _intervalBar(){
-    return  Column(
+   return Column(
       children: [
-        _interval(),
+        _interval(customColor: const Color.fromARGB(255, 66, 144, 255)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 94, 173, 167)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 118, 188, 116)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 145, 186, 62)),
+        _separator(), 
+        _interval(customColor: const Color.fromARGB(255, 169, 183, 60)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 192, 181, 60)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 230, 190, 64)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 237, 169, 59)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 235, 138, 60)),
+        _separator(),
+        _interval(customColor: const Color.fromARGB(255, 229, 95, 72)),
       ],
-
     );
   }
   
-  Widget _label() {
-    return const Text(
-      '1.0',
-    );
+   Widget _separator(){
+    return const SizedBox(
+          height: 2.2,
+        );
   }
 
-  Widget _interval(){
-    //const Colors color = colors[colorType];
-    return const SizedBox(
-      width: 42.0,
-      height: 10.0,
-      child:  DecoratedBox(
-        //decoration = decoration ?? (color != null ? BoxDecoration(color: color) : null),
+
+  Widget _label() {
+    return 
+        Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              '1.0',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ],
+        );
+  }
+
+   Widget _interval({required Color customColor}){
+    return  SizedBox(
+      width: 15.0,
+      height: 3.8,
+      child: DecoratedBox(
         decoration: BoxDecoration(
-          color: Colors.amber,
+          color: customColor,
+        )
         ),
-      ),
     );
   }
   
